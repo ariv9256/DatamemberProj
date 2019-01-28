d5fa59ea8f45d40bcc693cc81a4ba9c82144ec0b
Angela Rivas <snowcats001@gmail.com>
2019-01-28 12:14:51 -0700

    Deleted main class and added DataController HPP & CPP and DataRunner CPP classes

---
+++ DatamemberProj/Controller/DataController.hpp
@@ -0,0 +1,14 @@
+//
+//  DataController.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 1/28/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef DataController_hpp
+#define DataController_hpp
+
+#include <stdio.h>
+
+#endif /* DataController_hpp */
---
+++ DatamemberProj/Controller/DataRunner.cpp
@@ -0,0 +1,9 @@
+//
+//  DataRunner.cpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 1/28/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#include <stdio.h>
