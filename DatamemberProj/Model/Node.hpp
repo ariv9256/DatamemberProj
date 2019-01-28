27426210e6c16e7462a662ce6e711258a8eead38
Angela Rivas <snowcats001@gmail.com>
2019-01-28 13:12:36 -0700

    Added a "this.blank" variable in C++ Node class for data.

---
+++ DatamemberProj/Model/Node.hpp
@@ -0,0 +1,45 @@
+//
+//  Node.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 1/28/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <assert.h>
+
+template <class Type>
+class Node
+{
+private:
+    Type data;
+public:
+    Node(Type data);
+    Type getData();
+    void setData(Type data);
+}
+
+//Template definitions
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->data;
+}
+
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
+
+template <class Type>
+void Node<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+
+#endif /* Node_h */
