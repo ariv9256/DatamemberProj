278b534865f134d5bf3a086f70af255493584488
Angela Rivas <snowcats001@gmail.com>
2019-01-30 13:01:08 -0700

    Added arrayhpp class

---
+++ DatamemberProj/Model/Linear/Array.h
@@ -0,0 +1,14 @@
+//
+//  Array.h
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 1/30/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef Array_h
+#define Array_h
+
+
+
+#endif /* Array_h */
