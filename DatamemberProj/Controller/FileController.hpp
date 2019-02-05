e29b12f1924da1cd14c141b1cd483ab2d8a09c40
Angela Rivas <snowcats001@gmail.com>
2019-02-05 13:01:47 -0700

    Added FileController class

--- DatamemberProj/Controller/FileController.hpp
+++ DatamemberProj/Controller/FileController.hpp
@@ -10,13 +10,20 @@
 #define FileController_hpp
 
 #include <iostream>
-#include "../Resources/CrimeData.hpp"
 #include "../Resources/Music.hpp"
+#include "../Resources/CrimeData.hpp"
 #include <string>
 #include <fstream>
 #include <vector>
 #include <sstream>
 
 using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+    static vector<Music> musicDataToVector(string filename);
+}
 
 #endif /* FileController_hpp */
