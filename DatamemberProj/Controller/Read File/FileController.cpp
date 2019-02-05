6b79f0af308453b42086345513e9ab0dffe8cdb5
Angela Rivas <snowcats001@gmail.com>
2019-02-05 12:36:49 -0700

    Created new File controllers for new ReadFile project and added all include statements

---
+++ DatamemberProj/Controller/Read File/FileController.cpp
@@ -0,0 +1,9 @@
+//
+//  FileController.cpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 2/5/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#include "FileController.hpp"
---
+++ DatamemberProj/Controller/Read File/FileController.hpp
@@ -0,0 +1,20 @@
+//
+//  FileController.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 2/5/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include <iostream>
+#include "../Resources/CrimeData.hpp"
+#include "../Resources/Music.hpp"
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+#endif /* FileController_hpp */
