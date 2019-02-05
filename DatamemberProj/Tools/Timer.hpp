c6c452b8d7949af5e04c12b874f6072dcea0717e
Angela Rivas <snowcats001@gmail.com>
2019-02-05 12:11:36 -0700

    Added timer hpp cpp files in Tools folders

---
+++ DatamemberProj/Tools/Timer.hpp
@@ -0,0 +1,14 @@
+//
+//  Timer.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 2/5/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef Timer_hpp
+#define Timer_hpp
+
+#include <stdio.h>
+
+#endif /* Timer_hpp */
