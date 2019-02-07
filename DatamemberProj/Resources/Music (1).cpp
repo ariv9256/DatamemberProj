c373b91fac6dd12c5f96fcfb33499f75d5c9d44d
Angela Rivas <snowcats001@gmail.com>
2019-02-07 14:17:55 -0700

    Deleted a breakpoint (not important)

--- DatamemberProj/Resources/Music (1).cpp
+++ DatamemberProj/Resources/Music (1).cpp
@@ -77,7 +77,7 @@ Music :: Music(string data)
     this->release_name = release_name;
     this->similar = similar;
     this->song_hotttnesss = (stod(song_hotttnesss));
-    this->song_id = (stoi(song_id));
+    this->song_id = (song_id);
     this->start_of_fade_out = (stod(start_of_fade_out));
     this->tatums_confidence = (stod(tatums_confidence));
     this->tatums_start = (stod(tatums_start));
