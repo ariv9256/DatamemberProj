d9d07e5beb78ef2d459bcc988605cc3c9ec35540
Angela Rivas <snowcats001@gmail.com>
2019-02-13 13:48:30 -0700

    Added Timer for Music Data and vector. Also added all variables and include statements in tester hpp file

--- DatamemberProj/Testers/LinearTester.cpp
+++ DatamemberProj/Testers/LinearTester.cpp
@@ -10,17 +10,29 @@
 
 void LinearTester :: testVsSTL()
 {
-    Timer crimeTimerStl, crimeTimerOOP, musicSTL, musicOOP;
+    Timer crimeTimerSTL, crimeTimerOOP, musicSTL, musicOOP;
     
-    crimeTimer.startTimer();
-    vector<CrimeData> crimes = FileController :: readCrimeDataToVector("");
+    crimeTimerSTL.startTimer();
+    vector<CrimeData> crimes = FileController :: readCrimeDataToVector("/Users/ariv9256/Documents/C++ Projects/DatamemberProj/DatamemberProj/Resources/crime.csv");
     crimeTimerSTL.stopTimer();
     
     crimeTimerOOP.startTimer();
-    LinkedList<CrimeData> moreCrimes = FileController :: readDataToList("");
+    LinkedList<CrimeData> moreCrimes = FileController :: readDataToList("/Users/ariv9256/Documents/C++ Projects/DatamemberProj/DatamemberProj/Resources/crime.csv");
     crimeTimerOOP.stopTimer();
     
     crimeTimerSTL.displayInformation();
     crimeTimerOOP.displayInformation();
     cout<< "A difference of: " << crimeTimerOOP.getTimeInMicroseconds() - crimeTimerSTL.getTimeInMicroseconds() << " microseconds" << endl;
+    
+    musicSTL.startTimer();
+    vector<Music> tunez = FileController :: musicDataToVector("/Users/ariv9256/Documents/C++ Projects/DatamemberProj/DatamemberProj/Resources/music.csv");
+    musicSTL.stopTimer();
+    
+    musicOOP.startTimer();
+    LinkedList<Music> musicList = FileController :: musicDataToList("/Users/ariv9256/Documents/C++ Projects/DatamemberProj/DatamemberProj/Resources/music.csv");
+    musicOOP.stopTimer();
+    
+    musicSTL.displayInformation();
+    musicOOP.displayInformation();
+    cout<< "A difference of: " << musicOOP.getTimeInMicroseconds() - musicSTL.getTimeInMicroseconds() << " microseconds" << endl;
 }
--- DatamemberProj/Testers/LinearTester.hpp
+++ DatamemberProj/Testers/LinearTester.hpp
@@ -13,6 +13,18 @@
 #include "../Controller/Tools/Timer.hpp"
 #include "../Controller/FileController.hpp"
 
+#include "../Model/Linear/LinkedList.hpp"
+#include "../Model/Linear/Stack.hpp"
+#include "../Model/Linear/Queue.hpp"
+#include "../Model/Linear/Array.h"
+
+using namespace std;
+
+class LinearTester
+{
+public:
+    void testVsSTL();
+};
 
 
 #endif /* LinearTester_hpp */
