2ecf073f0031b2763fb2e1cc24f316b33a24be9a
Angela Rivas <snowcats001@gmail.com>
2019-02-13 12:44:38 -0700

    Added Queue class and added header & includes

---
+++ DatamemberProj/Model/Linear/Queue.hpp
@@ -0,0 +1,23 @@
+//
+//  Queue.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 2/13/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include <iostream>
+#include "LinkedList.hpp"
+
+using namespace std;
+
+template <class Type>
+class Queue : public LinkedList<Type>
+{
+    
+}
+
+#endif /* Queue_hpp */
