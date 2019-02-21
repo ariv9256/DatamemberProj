84715d41299258012b5d8f1d9c60ae4c4b469305
Angela Rivas <snowcats001@gmail.com>
2019-02-21 12:15:21 -0700

    Created CircularList class & deleted cpp file

---
+++ DatamemberProj/Model/Nodes/CircularList.hpp
@@ -0,0 +1,14 @@
+//
+//  CircularList.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 2/21/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef CircularList_hpp
+#define CircularList_hpp
+
+#include <stdio.h>
+
+#endif /* CircularList_hpp */
