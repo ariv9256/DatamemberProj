a86f00d935f45d8528baa339a47200f5452b2f9e
Angela Rivas <snowcats001@gmail.com>
2019-02-21 11:52:52 -0700

    Added a doublenode class

---
+++ DatamemberProj/Model/Nodes/DoubleNode.hpp
@@ -0,0 +1,18 @@
+//
+//  DoubleNode.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 2/21/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef DoubleNode_hpp
+#define DoubleNode_hpp
+
+#include <iostream>
+#include "Node.hpp"
+
+template<class Type>
+class DoubleNode : public Node<Type>
+
+#endif /* DoubleNode_hpp */
