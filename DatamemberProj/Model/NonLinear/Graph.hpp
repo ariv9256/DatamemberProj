7dc8fd103cdca6b748bd604bd5759fdbc6462477
Angela Rivas <snowcats001@gmail.com>
2019-03-04 13:55:03 -0700

    Added all private and public variables for Graph

--- DatamemberProj/Model/NonLinear/Graph.hpp
+++ DatamemberProj/Model/NonLinear/Graph.hpp
@@ -18,6 +18,42 @@ using namespace std;
 template <class Type>
 class Graph
 {
+private:
+    static const int MAXIMUM = 50;
+    bool adjacencyMatrix [MAXIMUM][MAXIUMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    //Graph operations
+    
+    void addVertex(const Type & value);
+    
+    //Connect vertices
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target);
+    //Disconnect vertices
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //Accessors
+    Type & operator [] (int vertex);
+    Type operator[] (int vertext) const;
+    int size() const;
+    
+    //Check connections
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target) const;
+    std:::set<int> neighbors(int vertex) const;
+    
+    //Traversals
+    void depthFirstTraversal(Graph<Type> & graph, int vertext);
+    void breadthFirstTraversal(Graph<TYpe> & graph, int vertex);
+    int costTraversal(Graph <Type> & graph, int vertex); 
     
 };
 #endif /* Graph_hpp */
