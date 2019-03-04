b2f73a430de473e2e32f27708d8fff80f9b218cb
Angela Rivas <snowcats001@gmail.com>
2019-03-04 13:48:22 -0700

    Added Graph class and added it into a newly created NonLinear group

---
+++ DatamemberProj/Model/NonLinear/Graph.hpp
@@ -0,0 +1,23 @@
+//
+//  Graph.hpp
+//  DatamemberProj
+//
+//  Created by Rivas, Angela on 3/4/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+
+using namespace std;
+template <class Type>
+class Graph
+{
+    
+};
+#endif /* Graph_hpp */
