21eaba1be88fdbcbbafe603808fc4d5fe5e207f4
Angela Rivas <snowcats001@gmail.com>
2019-04-12 13:59:38 -0600

    Added all protected & public constructors in reference of tree class

--- DatamemberProj/Model/Linear/List.hpp
+++ DatamemberProj/Model/Linear/List.hpp
@@ -11,7 +11,6 @@
 
 #include <iostream>
 #include "../Nodes/LinearNode.hpp"
-#include <assert.h>
 
 template <class Type>
 class List
--- DatamemberProj/Model/NonLinear/BinarySearchTree.hpp
+++ DatamemberProj/Model/NonLinear/BinarySearchTree.hpp
@@ -9,6 +9,52 @@
 #ifndef BinarySearchTree_hpp
 #define BinarySearchTree_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include "Tree.hpp"
+
+using namespace std;
+
+template<class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * startNode, int index, int size);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    void destroyTree(BinaryTreeNode<Type> * node);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
+    
+    void removeNode(BinaryTreeNode<Type> * removeMe);
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinaryTreeNode<Type> * getRoot();
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    void demoTraversalSteps(BinaryTreeNode<Type> * node);
+    
+    int getSizee();
+    int getHeight();
+    bool isComplete();
+    bool isBalanced();
+    
+    bool contains(Type value);
+    void insert(Type itemToInsert);
+    void remove(Type value);
+    
+    Type findMinimum();
+    Type findMaximum();
+}
 
 #endif /* BinarySearchTree_hpp */
